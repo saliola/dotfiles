67ebc4a69d57196c35f4d346a40bf8825043ef8b
Franco Saliola <saliola@gmail.com>
2025-02-17 11:58:26 -0500

    update installation instructions (README) + github-ssh-keygen

--- README.md
+++ README.md
@@ -2,4 +2,25 @@
 
 ## Installation
 
-Clone repo and run `install.sh`.
+1. Generate a new ssh key to github; the process is automated by the
+   `github-ssh-keygen.sh` script:
+```
+curl https://raw.githubusercontent.com/driesvints/dotfiles/HEAD/github-ssh-keygen.sh | sh -s "saliola@gmail.com"
+```
+
+2. Add the new ssh key to github.
+
+3. Clone this repo:
+```
+git clone git@github.com:saliola/dotfiles.git ~/.dotfiles
+```
+
+3. Run the installation script:
+```
+cd ~/.dotfiles
+./install.sh
+```
+
+## References
+
+- [Getting Started with Dotfiles](https://driesvints.com/blog/getting-started-with-dotfiles)
---
+++ github-ssh-keygen.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Reference: https://github.com/driesvints/dotfiles/blob/main/ssh.sh
+
+echo "Generating a new SSH key for GitHub..."
+
+# Generating a new SSH key
+# https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#generating-a-new-ssh-key
+ssh-keygen -t ed25519 -C $1 -f ~/.ssh/id_ed25519
+
+# Adding your SSH key to the ssh-agent
+# https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#adding-your-ssh-key-to-the-ssh-agent
+eval "$(ssh-agent -s)"
+
+touch ~/.ssh/config
+echo "Host *\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_ed25519" | tee ~/.ssh/config
+
+ssh-add --apple-use-keychain ~/.ssh/id_ed25519
+
+# Adding your SSH key to your GitHub account
+# https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account#adding-a-new-ssh-key-to-your-account
+echo "run 'pbcopy < ~/.ssh/id_ed25519.pub' and paste that into github.com"
