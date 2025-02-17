2e52670001ae63e5710a9fbee722e3ad78b4e1e0
Franco Saliola <saliola@gmail.com>
2025-02-17 17:47:20 -0500

    temporarily disable ChatGPT plugin for neovim

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -52,20 +52,20 @@ return {
         end
     },
 
-    {
-        "jackMort/ChatGPT.nvim",
-        event = "VeryLazy",
-        dependencies = {
-            "MunifTanjim/nui.nvim",
-            "nvim-lua/plenary.nvim",
-            "nvim-telescope/telescope.nvim"
-        },
-        config = function()
-            require("chatgpt").setup({
-                api_key_cmd = "op read op://APIKeys/OpenAI/credential --no-newline"
-            })
-        end
-    },
+    -- {
+    --     "jackMort/ChatGPT.nvim",
+    --     event = "VeryLazy",
+    --     dependencies = {
+    --         "MunifTanjim/nui.nvim",
+    --         "nvim-lua/plenary.nvim",
+    --         "nvim-telescope/telescope.nvim"
+    --     },
+    --     config = function()
+    --         require("chatgpt").setup({
+    --             api_key_cmd = "op read op://APIKeys/OpenAI/credential --no-newline"
+    --         })
+    --     end
+    -- },
 
     {
         'saghen/blink.cmp',
