c772f21051eb0a388f99281a323bde1852175714
Franco Saliola <saliola@gmail.com>
2025-02-17 12:42:18 -0500

    import Brewfile

--- install.sh
+++ install.sh
@@ -6,6 +6,23 @@ set -x
 # exit script at first error
 set -e
 
+
+echo "Installing apps via brew..."
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Update Homebrew recipes
+brew update
+
+# Install packages listed in Brewfile
+brew tap homebrew/bundle
+brew bundle --file ./Brewfile
+
+
+echo "Using stow to symlink configs..."
 # tmux
 stow -v -t ~ tmux
 git clone git@github.com:tmux-plugins/tpm.git ~/.tmux/plugins/tpm
