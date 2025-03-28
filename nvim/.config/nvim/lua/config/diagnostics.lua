d8d0ca5175ce711054d7c31da1bd6f972b5d9d00
Franco Saliola <saliola@gmail.com>
2025-03-28 10:17:41 -0400

    use vim.diagnostic.config instead of sign_define (deprecated)

--- nvim/.config/nvim/lua/config/diagnostics.lua
+++ nvim/.config/nvim/lua/config/diagnostics.lua
@@ -16,8 +16,22 @@ for _, hl in ipairs(hl_groups) do
 end
 
 -- change symbols for DiagnosticSign
-local symbols = { Error = "󰅙", Info = "󰋼", Hint = "󰌵", Warn = "" }
-for name, icon in pairs(symbols) do
-    local hl = "DiagnosticSign" .. name
-    vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
-end
+vim.diagnostic.config({
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "󰅙",
+            [vim.diagnostic.severity.INFO]  = "󰋼",
+            [vim.diagnostic.severity.HINT]  = "󰌵",
+            [vim.diagnostic.severity.WARN]  = "",
+        },
+        linehl = {
+            [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+        },
+        numhl = {
+            [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+            [vim.diagnostic.severity.INFO]  = "DiagnosticSignInfo",
+            [vim.diagnostic.severity.HINT]  = "DiagnosticSignHint",
+            [vim.diagnostic.severity.WARN]  = "DiagnosticSignWarn",
+        },
+    },
+})
