be151f737589d9247e7508436b455bf57ef78ecd
saliola <saliola@users.noreply.github.com>
2026-04-24 09:17:39 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,22 +11,26 @@ echo "Installing apps via apt..."
 
 sudo apt install stow
 sudo apt install tmux
-sudo apt install npm
-sudo apt install tree-sitter-cli
+sudo apt install bash-completion eza htop ncdu
 
-sudo snap install neovim --classic
-sudo snap install hub --classic
+sudo apt install gh git-lfs hub
 
+sudo snap install nvim --classic
+sudo apt install nodejs npm
+sudo apt install fd-find fzf ripgrep tree
+sudo apt remove tree-sitter-cli
+npm install -g tree-sitter-cli
 
 echo "Using stow to symlink configs..."
 
 # tmux
 stow -v -t ~ tmux
-git clone git@github.com:tmux-plugins/tpm.git ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
 ~/.tmux/plugins/tpm/scripts/install_plugins.sh
 
 # bash
-rm ~/.bashrc ~/.profile
+mv ~/.bashrc{,.orig}
+mv ~/.profile{,.orig}
 stow -v -t ~ bash
 
 # others
