c5146c6ce0f4d34cd0a453c166a6d8bcd26ca6ae
Franco Saliola <saliola@gmail.com>
2026-05-12 09:40:39 -0400

    add -c flag to datestamp script for compact (no spaces)

--- bash/.config/bash/datestamp.sh
+++ bash/.config/bash/datestamp.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 
 # Usage:
-# 
-# `datestamp report.txt` --> 2026-05-11 - report.txt
+#
+# `datestamp report.txt`    --> 2026-05-11 - report.txt
+# `datestamp -c report.txt` --> 2026-05-11-report.txt
+
+COMPACT=false
+
+while getopts "c" opt; do
+    case $opt in
+        c) COMPACT=true ;;
+        *) echo "Usage: $(basename $0) [-c] <filename>"; exit 1 ;;
+    esac
+done
+shift $((OPTIND - 1))
 
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $(basename $0) <filename>"
+    echo "Usage: $(basename $0) [-c] <filename>"
     exit 1
 fi
 
 file="$1"
 
-if [ ! -f "$file" ]; then
-    echo "Error: File '$file' not found."
+if [ ! -e "$file" ]; then
+    echo "Error: '$file' not found."
     exit 1
 fi
 
 date=$(date +%Y-%m-%d)
-new_file="${date} - ${file}"
+
+if [ "$COMPACT" = true ]; then
+    new_file="${date}-${file}"
+else
+    new_file="${date} - ${file}"
+fi
 
 mv -i "$file" "$new_file"
-echo "File '$file' moved to '$new_file'"
+echo "'$file' moved to '$new_file'"
