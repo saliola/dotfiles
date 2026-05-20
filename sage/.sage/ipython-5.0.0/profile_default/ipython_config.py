1f8628b9838e4dc79515f0d90398d48c4137c65c
Franco Saliola <saliola@gmail.com>
2026-05-20 16:39:31 -0400

    import sage config (vi keybindings for sage terminal)

---
+++ sage/.sage/ipython-5.0.0/profile_default/ipython_config.py
@@ -0,0 +1,8 @@
+# Initialize the configuration object
+c = get_config()
+
+# Enable vi keybindings
+c.TerminalInteractiveShell.editing_mode = 'vi'
+
+# Optional but recommended: Removes the delay when pressing ESC
+c.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
